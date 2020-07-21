7fd1d5159ee098b5e262e747f02828f02516ee03
robert <robert.langereis@gmail.com>
2020-07-21 11:20:44 +0200

    lax in quotes

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import FileGrabber from './FileGrabber/fileGrabber';
 function App() {
   const cookies = new Cookies();
   const cookieList = ['currency', 'CB_URL', 'sc_is_visitor_unique', '_derived_epik', '_pin_unauth', 'gr_session', '_gid', 'country']
-  cookieList.forEach(cookie => cookies.remove(cookie, { sameSite: lax }))
+  cookieList.forEach(cookie => cookies.remove(cookie, { sameSite: 'lax' }))
 
   return (
     <FileGrabber/>
