c0b93f299df9dcd67c164daa2c70f41ccf2c7c02
robert <robert.langereis@gmail.com>
2020-07-21 12:02:17 +0200

    final attempt cookie removal third party

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
-import Cookies from 'universal-cookie';
-import FileGrabber from './FileGrabber/fileGrabber';
+import React from 'react'
+import './App.css'
+import Cookies from 'universal-cookie'
+import FileGrabber from './FileGrabber/fileGrabber'
 
-
-
-function App() {
-  const cookies = new Cookies();
-  const cookieList = ['currency', 'CB_URL', 'sc_is_visitor_unique', '_derived_epik', '_pin_unauth', 'gr_session', '_gid', 'country']
-  cookieList.forEach(cookie => cookies.remove(cookie, { sameSite: 'lax' }))
-
-  return (
-    <FileGrabber/>
-    );
+function App () {
+  const cookies = new Cookies()
+  const cookieList = [
+    'currency',
+    'CB_URL',
+    'sc_is_visitor_unique',
+    '_derived_epik',
+    '_pin_unauth',
+    'gr_session',
+    '_gid',
+    'country'
+  ]
+  // var d = new Date()
+  // const yesterday = d.setDate(d.getDate() - 5)
+  cookieList.forEach(cookie =>
+    cookies.remove(cookie, {
+      path: '/',
+      domain: '.flaticon.com'
+    })
+  )
+  return <FileGrabber />
 }
 
-export default App;
+export default App
